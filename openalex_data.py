dd654e67b5f9bdd5d4eeade19fe03320f40e27be
Alperen Ağa <alperencode@gmail.com>
2024-10-14 00:40:19 +0300

    feat: add basic expert finding query

--- openalex_data.py
+++ openalex_data.py
@@ -1,4 +1,4 @@
-from src import fetch, mongodb
+from src import fetch, mongodb, search
 
 # OpenAlex API
 API_URL = "https://api.openalex.org/works"
@@ -8,19 +8,35 @@ PARAMS = {
     "per_page": 50
 }
 
+
 if __name__ == "__main__":
     # Connect to db and collection
-    mongodb.connect_db_collection(
+    collection = mongodb.connect_db_collection(
         connection_string="mongodb://localhost:27017/",
         db_string="openalexdb",
-        collection="works",
+        collection_string="works",
     )
+
+    # Determine the search topic
+    topic = "clickbait"
 
     # Fetch works
     works = fetch.fetch_works(API_URL, PARAMS)
-
-    # Add works to db
     if works:
-        mongodb.add_to_mongodb(works)
+        # Add works to db [excluding duplicates]
+        mongodb.add_to_mongodb(collection, works)
+
+        # Find experts on "clickbait"
+        experts = search.find_experts(collection, topic)
+        if experts:
+            for expert in experts:
+                print(expert)
+
+        else:
+            print("No experts found.")
     else:
         print("No works found or an error occurred.")
+
+    print(f"\nParsed object: {len(works)}")
+    print(f"Parsed fields: {len(works[0].keys()) * len(works)}")
+    print(f"Experts found: {len(experts)}")
---
+++ src/search.py
@@ -0,0 +1,34 @@
+
+
+def find_experts(collection, topic):
+    # Query explanation:
+    # Searches topic in: "title", "abstract"
+    # Gets articles that has at least 5 citations
+    query = {
+        "$or": [
+            {"title": {"$regex": topic, "$options": "i"}},
+            {"abstract": {"$regex": topic, "$options": "i"}},
+            {"cited_by_count": {"$gte": 10000}}
+        ]
+    }
+
+    # Query operators explanation:
+    # `$or`: allows specify multiple conditions.
+    # `"$options": "i"`: Makes the regex case-insensitive
+    # `"$gte": 10000`: greater than or equal to 10.000
+
+    works = list(collection.find(query))
+
+    experts = [
+        {
+            "name": authorship['raw_author_name'],
+            "title": work['title'],
+            "id": work['id'],
+        }
+        for work in works
+        if 'authorships' in work
+        for authorship in work['authorships']
+        if authorship["author_position"] == "first"
+    ]
+
+    return list(experts)
